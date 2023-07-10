2fc0544fc74cb4d372be88121d99ab97714bc0d1
KalyaniNakhate <kalyani.nakhate@gmail.com>
2023-07-10 16:37:13 -0400

    Adding in main branch

---
+++ elif.py
@@ -0,0 +1,10 @@
+a = 30
+b = 30
+if b>a:
+    print("b is greater than a")
+elif a < b:
+    print("a and b are equal")
+elif a>b:
+    print("a is greater")
+else:
+    print("all above conditions are false.....")
